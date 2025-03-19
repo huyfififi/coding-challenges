771870dad41701764bca794bd828540c772515cf
Kazuki Kijima <kazukiyoshida96@gmail.com>
2025-03-18 23:29:56 -0700

    Assign a variable for a value used twice

--- leetcode/1/step2.py
+++ leetcode/1/step2.py
@@ -6,11 +6,11 @@ class Solution:
         # Time complexity: O(n), Space complexity: O(n)
 
         index_map = {}  # {num, index}
-        
+
         for i, num in enumerate(nums):
-            if target - num in index_map:
-                return [i, index_map[target - num]]
+            if (complement_i := index_map.get(target - num)) is not None:
+                return [i, complement_i]
             else:
                 index_map[num] = i
-        
+
         return [-1, -1]  # This should not happen
