c0519514d78e6835a9cd0a8789d090359130106e
Kazuki Kijima <kazukiyoshida96@gmail.com>
2025-03-31 23:09:00 -0700

    Revert changes with formatter (black)
    to keep method signature as it is

--- leetcode/21/step1.py
+++ leetcode/21/step1.py
@@ -4,11 +4,10 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(
-        self, list1: Optional[ListNode], list2: Optional[ListNode]
-    ) -> Optional[ListNode]:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         head = None
         curr = None
+
         while list1 and list2:
             if list1.val < list2.val:
                 if head is None:
