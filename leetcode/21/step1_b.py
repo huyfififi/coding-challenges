a3ca30381e3bc9a00772d2032e8dbe099d25012c
Kazuki Kijima <kazukiyoshida96@gmail.com>
2025-04-01 21:15:58 -0700

    Update the recursive approach

--- leetcode/21/step1_b.py
+++ leetcode/21/step1_b.py
@@ -5,24 +5,14 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
 
-        def splice(
-            prev: ListNode, node1: ListNode | None, node2: ListNode | None
-        ) -> None:
-            if node1 is None:
-                prev.next = node2
-                return
-            if node2 is None:
-                prev.next = node1
-                return
-
-            if node1.val < node2.val:
-                prev.next = node1
-                splice(node1, node1.next, node2)
-            else:
-                prev.next = node2
-                splice(node2, node1, node2.next)
-
-        splice(dummy, list1, list2)
-        return dummy.next
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
