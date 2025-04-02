9b25352a5d04cd251e051a48d5c931b1fbe626f8
Kazuki Kijima <kazukiyoshida96@gmail.com>
2025-04-01 21:16:07 -0700

    Fix wrong upload

--- leetcode/21/step2.py
+++ leetcode/21/step2.py
@@ -5,37 +5,21 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = None
-        curr = None
+        dummy = ListNode()
+        curr = dummy
 
-        while list1 and list2:
+        while list1 is not None and list2 is not None:
             if list1.val < list2.val:
-                if head is None:
-                    head = list1
-                    curr = list1
-                else:
-                    curr.next = list1
-                    curr = curr.next
+                curr.next = list1
                 list1 = list1.next
             else:
-                if head is None:
-                    head = list2
-                    curr = list2
-                else:
-                    curr.next = list2
-                    curr = curr.next
-                list2 = list2.next
-
-        if list1:
-            if head:
-                curr.next = list1
-            else:
-                head = list1
-
-        if list2:
-            if head:
                 curr.next = list2
-            else:
-                head = list2
+                list2 = list2.next
+            curr = curr.next
 
-        return head
+        if list1 is not None:
+            curr.next = list1
+        if list2 is not None:
+            curr.next = list2
+
+        return dummy.next
