0c5b92281bb62038d6c47d65c1c23c718b3575eb
Kazuki Kijima <kazukiyoshida96@gmail.com>
2025-04-21 00:03:13 -0700

    Add step 3 solutions

---
+++ leetcode/733/step3_bfs.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+
+class Solution:
+    def floodFill(
+        self, image: list[list[int]], sr: int, sc: int, color: int
+    ) -> list[list[int]]:
+        color_to_replace = image[sr][sc]
+        if color_to_replace == color:
+            return image
+
+        queue = deque([(sr, sc)])
+        num_rows, num_cols = len(image), len(image[0])
+
+        while queue:
+            row, col = queue.popleft()
+
+            if not (0 <= row < num_rows and 0 <= col < num_cols):
+                continue
+            if image[row][col] != color_to_replace:
+                continue
+
+            image[row][col] = color
+            queue.append((row - 1, col))
+            queue.append((row + 1, col))
+            queue.append((row, col - 1))
+            queue.append((row, col + 1))
+
+        return image
