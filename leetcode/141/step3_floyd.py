d865ace06d75761bc29292ed9fb742ad8f617488
Kazuki Kijima <kazukiyoshida96@gmail.com>
2025-05-04 17:33:36 -0700

    Add step 3 solutions

---
+++ leetcode/141/step3_floyd.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow_runner = head
+        fast_runner = head
+
+        while fast_runner is not None and fast_runner.next is not None:
+            slow_runner = slow_runner.next
+            fast_runner = fast_runner.next.next
+            if slow_runner is fast_runner:
+                return True
+
+        return False
