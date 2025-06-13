4728e7f2aaf00730f2347e7ae08c44c132d85c32
Kazuki Kijima <kazukiyoshida96@gmail.com>
2025-06-12 22:27:49 -0700

    LeetCode 169: Update Quickselect Solution

---
+++ leetcode/169/step1_quickselect.py
@@ -0,0 +1,27 @@
+import random
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        def quickselect(nums: list[int], left: int, right: int, k: int) -> int:
+            if left == right:
+                return nums[left]
+
+            pivot_index = random.randint(left, right)
+            pivot_value = nums[pivot_index]
+            nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
+            store_index = left
+            for i in range(left, right):
+                if nums[i] >= pivot_value:
+                    nums[i], nums[store_index] = nums[store_index], nums[i]
+                    store_index += 1
+            nums[right], nums[store_index] = nums[store_index], nums[right]
+
+            if store_index == k:
+                return nums[store_index]
+            elif store_index < k:
+                return quickselect(nums, store_index + 1, right, k)
+            else:
+                return quickselect(nums, left, store_index - 1, k)
+
+        return quickselect(nums, 0, len(nums) - 1, len(nums) // 2)
