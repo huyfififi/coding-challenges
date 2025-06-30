dd0bfbbac016f6f97ce3efe90e3e46ad8284105f
Kazuki Kijima <kazukiyoshida96@gmail.com>
2025-06-30 00:13:06 -0700

    LeetCode 53: Tweak a solution

--- leetcode/53/step3_kadane.py
+++ leetcode/53/step3_kadane.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
-        max_ending_sum = float("-inf")
+        max_ending_sum = 0
         max_sum = float("-inf")
         for num in nums:
             max_ending_sum = max(max_ending_sum + num, num)
