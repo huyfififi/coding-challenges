785f6eb944847635ae177084b10c7ca0c180600f
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-01-02 14:52:07 -0800

    LeetCode 207: Add step 1 solution with DFS

---
+++ leetcode/207/step1_dfs.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        course_to_prerequisites = {course: set() for course in range(numCourses)}
+        for course, prerequisite in prerequisites:
+            course_to_prerequisites[course].add(prerequisite)
+
+        visited = set()
+
+        def has_cycle(course: int, visiting: set[int]) -> bool:
+            if course in visited:
+                return False
+            if course in visiting:
+                return True
+
+            visiting.add(course)
+            for prerequisite in course_to_prerequisites[course]:
+                if has_cycle(prerequisite, visiting):
+                    return True
+
+            visited.add(course)
+            return False
+
+        for course in range(numCourses):
+            if has_cycle(course, set()):
+                return False
+
+        return True
