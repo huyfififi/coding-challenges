01a973ab4e8269e91983257f0023a35bb5d14c1e
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-01-09 21:24:45 -0800

    LeetCode 200: Add step 2 solution

---
+++ leetcode/200/step2.cpp
@@ -0,0 +1,45 @@
+#include <queue>
+#include <utility>
+#include <vector>
+
+class Solution {
+public:
+    int numIslands(const std::vector<std::vector<char>>& grid) {
+        constexpr char kWater = '0';
+        const int num_rows = (int)grid.size();
+        const int num_cols = (int)grid[0].size();
+        constexpr std::pair<int, int> kDirections[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+        int num_islands = 0;
+        std::vector<std::vector<bool>> visited(num_rows, std::vector<bool>(num_cols));
+
+        for (int row = 0; row < num_rows; ++row) {
+            for (int col = 0; col < num_cols; ++col) {
+                if (grid[row][col] == kWater) { continue; }
+                if (visited[row][col]) { continue; }
+
+                ++num_islands;
+
+                std::queue<std::pair<int, int>> area_to_visit;
+                area_to_visit.push({row, col});
+                while (!area_to_visit.empty()) {
+                    auto [r, c] = area_to_visit.front();
+                    area_to_visit.pop();
+                    if (r < 0 || num_rows <= r ||
+                        c < 0 || num_cols <= c) {
+                            continue;
+                        }
+                    if (grid[r][c] == kWater) { continue; }
+                    if (visited[r][c]) { continue; }
+
+                    for (auto [row_diff, col_diff] : kDirections) {
+                        area_to_visit.push({r + row_diff, c + col_diff});
+                    }
+                    visited[r][c] = true;
+                }
+            }
+        }
+
+        return num_islands;
+    }
+};
