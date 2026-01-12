083fed9d9a4dedaab3ea51112ce083d707518c04
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-01-11 23:38:04 -0800

    LeetCode 33: Add step 3 solution

---
+++ leetcode/33/step3.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+class Solution {
+public:
+    int search(const std::vector<int>& nums, int target) {
+        int first = 0;
+        int last = static_cast<int>(nums.size()) - 1;
+        while (first <= last) {
+            int middle = first + (last - first) / 2;
+            if (nums[middle] == target) { return middle; }
+            if (nums[first] <= nums[middle]) {
+                if (nums[first] <= target && target <= nums[middle]) {
+                    last = middle - 1;
+                } else {
+                    first = middle + 1;
+                }
+            } else {
+                if (nums[middle] <= target && target <= nums[last]) {
+                    first = middle + 1;
+                } else {
+                    last = middle - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
