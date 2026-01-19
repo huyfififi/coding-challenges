2b16252e29e5b1bedf25ea31b0ea22f257a05bca
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-01-19 11:43:42 -0800

    LeetCode 56: Tweak step 1 solution

--- leetcode/56/step1.cpp
+++ leetcode/56/step1.cpp
@@ -10,25 +10,22 @@ public:
 
         std::vector<std::vector<int>> merged_intervals;
 
-        int merging_start = sorted_intervals[0][0];
-        int merging_end = sorted_intervals[0][1];
-        size_t checking = 1;
-        while (checking < static_cast<int>(sorted_intervals.size())) {
-            int checking_start = sorted_intervals[checking][0];
-            int checking_end = sorted_intervals[checking][1];
-            if (merging_end < checking_start) {
-                merged_intervals.push_back({merging_start, merging_end});
-                merging_start = checking_start;
-                merging_end = checking_end;
-                ++checking;
-                continue;
-            }
+        int merged_start = sorted_intervals[0][0];
+        int merged_end = sorted_intervals[0][1];
+        for (size_t merging = 1; merging < sorted_intervals.size(); ++merging) {
+            int merging_start = sorted_intervals[merging][0];
+            int merging_end = sorted_intervals[merging][1];
 
-            merging_end = std::max(merging_end, checking_end);
-            ++checking;
+            if (merged_end < merging_start) {
+                merged_intervals.push_back({merged_start, merged_end});
+                merged_start = merging_start;
+                merged_end = merging_end;
+            } else {
+                merged_end = std::max(merged_end, merging_end);
+            }
         }
 
-        merged_intervals.push_back({merging_start, merging_end});
+        merged_intervals.push_back({merged_start, merged_end});
         return merged_intervals;
     }
 };
