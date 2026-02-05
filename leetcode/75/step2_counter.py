3f97f365d2e37b45827a55b5ef3b9420af7f194d
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-02-05 11:35:36 -0800

    LeetCode 75: Add step 2 solution with counter

---
+++ leetcode/75/step2_counter.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        RED = 0
+        WHITE = 1
+        BLUE = 2
+
+        counts = [0] * 3
+        for color in nums:
+            counts[color] += 1
+
+        filling = 0
+
+        def fill_color(color: int) -> None:
+            nonlocal filling
+            while counts[color] > 0:
+                nums[filling] = color
+                counts[color] -= 1
+                filling += 1
+
+        fill_color(RED)
+        fill_color(WHITE)
+        fill_color(BLUE)
