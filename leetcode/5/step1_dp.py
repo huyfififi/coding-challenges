684a9c19cea8613fbf58b1ce21a86cc5d17fd7b7
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-03-09 16:26:54 -0700

    LeetCode 5: Tweak step 1 DP solution

--- leetcode/5/step1_dp.py
+++ leetcode/5/step1_dp.py
@@ -1,25 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # is_palindrome[start][end] is True -> s[start:end + 1] is a palindrome
         is_palindrome = [[False] * len(s) for _ in range(len(s))]
         for i in range(len(s)):
             is_palindrome[i][i] = True
 
-        max_length = 1
         max_start = 0
-        max_end = 0
+        max_length = 1
         for start in range(len(s) - 1, -1, -1):
             for end in range(start, len(s)):
-                if (
-                    s[start] == s[end]
-                    and (
-                        end - start + 1 <= 2 or is_palindrome[start + 1][end - 1]
-                    )
-                ):
-                    is_palindrome[start][end] = True
-                    if end - start + 1 > max_length:
-                        max_length = end - start + 1
-                        max_start = start
-                        max_end = end
+                if s[start] != s[end]:
+                    continue
 
-        return s[max_start:max_end + 1]
+                length = end - start + 1
+                if length <= 2 or is_palindrome[start + 1][end - 1]:
+                    is_palindrome[start][end] = True
+                    if length > max_length:
+                        max_start = start
+                        max_length = length
+
+        return s[max_start : max_start + max_length]
