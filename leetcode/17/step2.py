65c3686a6601e65b0442c83073ec37b1a53cff8f
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-03-19 14:13:33 -0700

    LeetCode 17: Add step 2 solution

---
+++ leetcode/17/step2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        digit_to_characters: dict[str, list[str]] = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+
+        generated = []
+
+        def generate_combinations(digit_index: int, generating: list[int]) -> None:
+            if digit_index == len(digits):
+                generated.append("".join(generating))
+                return
+
+            for character in digit_to_characters[digits[digit_index]]:
+                generating.append(character)
+                generate_combinations(digit_index + 1, generating)
+                generating.pop()
+
+        generate_combinations(0, [])
+        return generated
