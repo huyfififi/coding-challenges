7fd51b3b03ebb0547b8de16d4ab996ee5fc21584
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-03-21 16:54:53 -0700

    LeetCode 79: Browsed thonda28-san's PR

---
+++ leetcode/79/step2_skip_redundant_recursions.py
@@ -0,0 +1,50 @@
+import collections
+import itertools
+
+
+class Solution:
+    def exist(self, board: list[list[str]], word: str) -> bool:
+        num_rows = len(board)
+        num_cols = len(board[0])
+
+        board_char_to_count = collections.Counter(itertools.chain.from_iterable(board))
+        word_char_to_count = collections.Counter(word)
+        for ch in word_char_to_count:
+            if board_char_to_count[ch] < word_char_to_count[ch]:
+                return False
+
+        if board_char_to_count[word[-1]] < board_char_to_count[word[0]]:
+            word = "".join(reversed(word))
+
+        def starts_from(
+            row: int, col: int, word_idx: int, used: list[list[bool]]
+        ) -> bool:
+            if word_idx == len(word):
+                return True
+
+            if not (0 <= row < num_rows and 0 <= col < num_cols):
+                return False
+            if used[row][col]:
+                return False
+            if board[row][col] != word[word_idx]:
+                return False
+
+            used[row][col] = True
+            if (
+                starts_from(row + 1, col, word_idx + 1, used)
+                or starts_from(row - 1, col, word_idx + 1, used)
+                or starts_from(row, col + 1, word_idx + 1, used)
+                or starts_from(row, col - 1, word_idx + 1, used)
+            ):
+                return True
+
+            used[row][col] = False
+            return False
+
+        used = [[False] * num_cols for _ in range(num_rows)]
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if starts_from(row, col, 0, used):
+                    return True
+
+        return False
