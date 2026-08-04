1693f866911fc133ee3e397bd47f2d4e1137d329
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-08-03 20:59:37 -0700

    LeetCode 621: Remove a redundant variable

--- leetcode/621/step2_batch_by_cycle.py
+++ leetcode/621/step2_batch_by_cycle.py
@@ -17,17 +17,14 @@ class Solution:
         time = 0
         cycle_length = n + 1
         while negated_counts_heap:
-            time_in_cycle = 0
-            num_tasks_done = 0
             next_counts = []
+            num_tasks_done = 0
 
-            while time_in_cycle < cycle_length and negated_counts_heap:
+            for _ in range(min(cycle_length, len(negated_counts_heap))):
                 count = -heapq.heappop(negated_counts_heap)
-                num_tasks_done += 1
                 if count > 1:
                     next_counts.append(count - 1)
-
-                time_in_cycle += 1
+                num_tasks_done += 1
 
             for count in next_counts:
                 heapq.heappush(negated_counts_heap, -count)
