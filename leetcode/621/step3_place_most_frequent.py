6cdc2aa8dcf92852a0ca22fba94cedcd483f0a41
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-08-09 23:11:59 -0700

    LeetCode 621: Add step 3 solution - place most frequent tasks first

---
+++ leetcode/621/step3_place_most_frequent.py
@@ -0,0 +1,21 @@
+import collections
+
+
+class Solution:
+    def leastInterval(self, tasks: list[str], n: int) -> int:
+        label_to_count = collections.Counter(tasks)
+
+        max_count = max(label_to_count.values())
+
+        num_most_frequent_labels = 0
+        for count in label_to_count.values():
+            if count == max_count:
+                num_most_frequent_labels += 1
+
+        num_gaps = max_count - 1
+        empty_slots_per_gap = max(0, n - (num_most_frequent_labels - 1))
+        empty_slots = num_gaps * empty_slots_per_gap
+
+        num_non_frequent_tasks = len(tasks) - max_count * num_most_frequent_labels
+
+        return len(tasks) + max(0, empty_slots - num_non_frequent_tasks)
