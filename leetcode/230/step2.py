b79d521817de43ed800aa9e230809696120883eb
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-08-30 18:04:08 -0700

    LeetCode 230: Add step 2 solution

---
+++ leetcode/230/step2.py
@@ -0,0 +1,28 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root: TreeNode | None, k: int) -> int:
+        count = 0
+
+        def find_kth_smallest(node: TreeNode | None) -> int | None:
+            nonlocal count
+
+            if node is None:
+                return None
+
+            kth_smallest = find_kth_smallest(node.left)
+            if kth_smallest is not None:
+                return kth_smallest
+
+            count += 1
+            if count == k:
+                return node.val
+
+            return find_kth_smallest(node.right)
+
+        return find_kth_smallest(root)
