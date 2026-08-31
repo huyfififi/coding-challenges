f819a66b0cb6204770ec1c1503e33de2a52eecd5
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-08-30 22:43:05 -0700

    LeetCode 450: Add solutions

---
+++ leetcode/230/450_step1_predecessor.py
@@ -0,0 +1,32 @@
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deleteNode(self, root: TreeNode | None, key: int) -> TreeNode | None:
+        if root is None:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            if root.right is None:
+                return root.left
+
+            predecessor = root.left
+            while predecessor.right is not None:
+                predecessor = predecessor.right
+
+            root.val = predecessor.val
+            root.left = self.deleteNode(root.left, predecessor.val)
+
+        return root
--- leetcode/230/memo.md
+++ leetcode/230/memo.md
@@ -28,7 +28,12 @@ Step 1 の解法をこれ以上速くするのは難しいのではないか、�
 
 `size` を追加した場合の BST への挿入と削除を書こうとしたが、削除で詰まってしまった。一旦、この問題は follow up 以前のものとして扱って、時間がある時に 450\. Delete Node in a BST に取り組みたいと思う。
 
-途中までのコード :arrow_down:
+450\. Deelte Node in a BST に取り組んできた。
+
+- pick largest in the left subtree -> `450_step1_predecessor.py`
+- pick smallest in the right subtree -> `450_step1_successor.py`
+
+Time Complexity は O(h)。一度目の predecessor (successor) の探索で h のノードを訪問。二度目の `deleteNode()` で`root.left` (`root.right`) から先ほどの predecessor (successor) まで行って (O(h)) そこで O(1) の繋ぎかえ。
 
 ```py
 from __future__ import annotations
