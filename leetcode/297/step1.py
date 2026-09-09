1fd933b12293111fcbf1ff940c8716c628f3b83f
Kazuki Kijima <kazukiyoshida96@gmail.com>
2026-09-08 18:44:40 -0700

    LeetCode 297: Asked AI for implementation idea

--- leetcode/297/step1.py
+++ leetcode/297/step1.py
@@ -1,80 +1,34 @@
-CHUNK_SIZE = 4
-
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 
 class Codec:
-    def serialize(self, root: TreeNode) -> str:
-        traversed: list[TreeNode | None] = []
-        nodes = [root]
-        while nodes and any(node for node in nodes if node is not None):
-            next_nodes = []
+    def serialize(self, root: TreeNode | None) -> str:
+        """Encodes a tree to a single string.
 
-            for node in nodes:
-                traversed.append(node)
-                if node is None:
-                    next_nodes.append(None)
-                    next_nodes.append(None)
-                    continue
-
-                next_nodes.append(node.left)
-                next_nodes.append(node.right)
-
-            nodes = next_nodes
-
+        :type root: TreeNode
+        :rtype: str
+        """
         serialized: list[str] = []
-        for node in traversed:
+
+        def traverse(node: TreeNode | None) -> None:
             if node is None:
-                serialized.append("9999")
-            else:
-                serialized.append(str(node.val + 1000).rjust(4, "0"))
+                serialized.append("#")
+                return
 
-        return "".join(serialized)
+            serialized.append(str(node.val))
+            traverse(node.left)
+            traverse(node.right)
 
-    def deserialize(self, data: str) -> TreeNode:
-        if not data:
-            return None
+        traverse(root)
+        return ",".join(serialized)
 
-        num_nodes = len(data) // CHUNK_SIZE
-        chunks = []
-        for i in range(num_nodes):
-            chunk_data = data[CHUNK_SIZE * i : CHUNK_SIZE * (i + 1)]
-            if chunk_data == "9999":
-                chunks.append(None)
-            else:
-                chunks.append(int(chunk_data) - 1000)
-
-        max_depth = 0
-        while num_nodes > 0:
-            max_depth += 1
-            num_nodes = num_nodes // 2
-
-        def build_tree(depth: int) -> list[TreeNode]:
-            if depth == max_depth:
-                return [None] * (2**depth)
-
-            child_nodes: list[TreeNode] = build_tree(depth + 1)
-            nodes = []
-            for order_i, chunk_i in enumerate(
-                range(2**depth - 1, 2 ** (depth + 1) - 1)
-            ):
-                if chunks[chunk_i] is None:
-                    nodes.append(None)
-                    continue
-
-                node = TreeNode(chunks[chunk_i])
-                node.left = child_nodes[2 * order_i]
-                node.right = child_nodes[2 * order_i + 1]
-                nodes.append(node)
-
-            return nodes
-
-        return build_tree(0)[0]
+    def deserialize(self, data: str) -> TreeNode | None:
+        raise NotImplementedError("TODO")
 
 
 # Your Codec object will be instantiated and called as such:
